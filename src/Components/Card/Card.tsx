4664c627a69dd691272e4d85bff7b91856fee986
Rosario <rosariosanchezdev@gmail.com>
2024-05-18 10:09:05 -0300

    api connected

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -6,7 +6,7 @@ interface Props {
     price: number;
 }
 
-const Card = ({companyName, ticker, price}: Props) => {
+const Card: React.FC<Props> = ({companyName, ticker, price}: Props): JSX.Element => {
   return (
     <div className='card'>
         <img src="https://images.unsplash.com/photo-1518895949257-7621c3c786d7?q=80&w=1376&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />

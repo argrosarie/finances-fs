888baa829eb5eb5ad0c0a0b4f854ad587eeff5b1
Rosario <rosariosanchezdev@gmail.com>
2024-05-22 09:02:30 -0300

    tailwind css

--- src/Components/Portfolio/DeletePortfolio/DeletePortfolio.tsx
+++ src/Components/Portfolio/DeletePortfolio/DeletePortfolio.tsx
@@ -7,12 +7,14 @@ interface Props {
 
 const DeletePortfolio = ({onPortfolioDelete, portfolioValue}: Props) => {
   return (
-    <>
+    <div>
     <form onSubmit={onPortfolioDelete}>
-        <input type="text" hidden={true} value={portfolioValue}/>
-        <button>X</button>
+      <input hidden={true} value={portfolioValue} />
+      <button className="block w-full py-3 text-white duration-200 border-2 rounded-lg bg-red-500 hover:text-red-500 hover:bg-white border-red-500">
+        X
+      </button>
     </form>
-    </>
+  </div>
   )
 }
 

1964177e0638509ea75d7a2a629a8a16eea4b8fc
Rosario <rosariosanchezdev@gmail.com>
2024-05-24 11:39:25 -0300

    companyPage api

--- src/Pages/CompanyPage/CompanyPage.tsx
+++ src/Pages/CompanyPage/CompanyPage.tsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router'
+import { CompanyProfile } from '../../company';
+import { getCompanyProfile } from '../../api';
 
-type Props = {}
+interface Props {}
 
 const CompanyPage = (props: Props) => {
+  let { ticker } = useParams();
+  const [company, setCompany] = useState<CompanyProfile>();
+
+  useEffect(()=> {
+    const getProfileInit = async () => {
+      const result= await getCompanyProfile(ticker!);
+      setCompany(result?.data[0]);
+    }
+    getProfileInit();
+  },[])
   return (
-    <div>CompanyPage</div>
+    <>
+    {company ? (
+      <div>{company?.companyName}</div>
+      ) : (
+        <div>company no found</div>
+      )}
+      </>
   )
 }
 
--- src/api.tsx
+++ src/api.tsx
@@ -1,12 +1,12 @@
 import axios from "axios"
-import { CompanySearch } from "./company"
+import { CompanyProfile, CompanySearch } from "./company"
 
 interface SearchResponse {
     data:CompanySearch[];
 }
 
 
-export const searchCompanies = async(query:string) => {
+export const searchCompanies = async (query:string) => {
     try {
         const data = await axios.get<SearchResponse>(
             `https://financialmodelingprep.com/api/v3/search-ticker?query=${query}&limit=10&exchange=NASDAQ&apikey=${process.env.REACT_APP_API_KEY}`
@@ -21,4 +21,22 @@ export const searchCompanies = async(query:string) => {
             return "unexpected error API no anda";
         }
     }
+}
+
+export const getCompanyProfile = async (query: string) => {
+    try {
+        const data = await axios.get<CompanyProfile[]>(
+            `https://financialmodelingprep.com/api/v3/profile/${query}?apikey=${process.env.REACT_APP_API_KEY}`
+        )
+        return data;
+    } catch (error:any) {
+         console.log("error message from API: ", error.message)
+    // if(axios.) {
+    //     return
+    // } else {
+    //     return
+    // }
+    }
+    
+   
 }
